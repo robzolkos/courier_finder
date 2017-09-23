cd75afe050ec8effa47f2674f31e5888b658ed85
Rob Zolkos <rob@zolkos.com>
2017-09-23 17:15:21 +1000

    Return false if a couriers please connote
    Australia Post and Couriers Please both have 14 digit connotes however
    Couriers Please prepends theirs with 'CP'

--- couriers/australia_post/index.test.js
+++ couriers/australia_post/index.test.js
@@ -16,6 +16,7 @@ describe("Australia Post", () => {
 
     describe("with an invalid connote", () => {
       test("returns false", () => {
+        expect(australia_post.valid("CP000000000000")).toBe(false); // 14 Couriers Please
         expect(australia_post.valid("12345")).toBe(false);
         expect(australia_post.valid("")).toBe(false);
         expect(australia_post.valid(null)).toBe(false);
