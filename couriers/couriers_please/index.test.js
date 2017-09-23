5074d16c6b9a8c2d942ae7401394387d21d4c090
Rob Zolkos <rob@zolkos.com>
2017-09-23 17:16:13 +1000

    Bugfix: Couriers Please connotes are 14 digits not 12

--- couriers/couriers_please/index.test.js
+++ couriers/couriers_please/index.test.js
@@ -4,12 +4,13 @@ describe("Couriers Please", () => {
   describe(".valid", () => {
     describe("with a valid connote", () => {
       test("returns true", () => {
-        expect(couriers_please.valid("CP0000E00000")).toBe(true);
+        expect(couriers_please.valid("CP0000E0000000")).toBe(true);
       });
     });
 
     describe("with an invalid connote", () => {
       test("returns false", () => {
+        expect(couriers_please.valid("CP000024716400")).toBe(false);
         expect(couriers_please.valid("CP0000247164")).toBe(false);
         expect(couriers_please.valid("12345")).toBe(false);
         expect(couriers_please.valid("")).toBe(false);
